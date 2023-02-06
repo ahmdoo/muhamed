e777d1b5b6bb5f5cddf76889711e2f944aa42438
ahmdoo <51219714+ahmdoo@users.noreply.github.com>
2023-02-07 02:57:51 +0300

    Create Muhamed-style.js

---
+++ Muhamed-style.js
@@ -0,0 +1,99 @@
+
+
+$("style").last().append(`
+#users .uzr.muh1 .fitimg.u-pic {
+  border-radius: 100px !important;
+  margin-top: 9px !important;
+  height: 52px !important;
+  margin-left: 13px!important;
+  z-index: 0 !important;
+}
+#users .uzr.muh1 .d-flex.dots {
+  margin-top: 7px !important;
+  margin-left: 5px;
+}
+#users .uzr.muh1 .u-msg {
+  text-align: center;
+  -webkit-background-clip: text;
+  padding: 1px !important;
+  font-size: 89% !important;
+  -webkit-text-fill-color: #0000;
+  background-image: url(https://i.ibb.co/JzgnvW8/61.gif);
+  color: transparent !important;
+  background-size: 90% 100%;
+}
+#users .uzr.muh1 .ustat {
+  width: 54px !important;
+  border-radius: 0px 50px 50px 50px !important;
+  height: 52px !important;
+  margin-top: 5px !important;
+  min-height: 0% !important;
+  margin-left: 10px;
+  z-index: 0 !important;
+  display: none;
+}
+#users .uzr.muh1 span.uhash {
+  margin-right: 324px;
+  margin-top: 24px;
+  margin-left: 0px;
+}
+#users .uzr.muh1 img.co {
+  transform: rotate(90deg);
+  margin-top: -60px !important;
+  margin-right: 282px;
+}
+#users .uzr.muh1 {
+  box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #000;
+  margin-bottom: 3px !important;
+  margin-top: 2px !important;
+  border-radius: 20px;
+  background-size: cover;
+  border: 1px solid #fff;
+  background-image: url("https://d.top4top.io/p_2593wv1s80.png");
+      background-size: cover;
+}
+#users .uzr.muh1 .c-flex.fr {
+  margin-left: -4px!important;
+  float: left;
+  background-color: #EDEDED;
+}
+#users .uzr.muh1 .d-flex.fl {
+  padding-right: 0px !important;
+}
+#users .uzr.muh1 .u-topic {
+  margin-left: 5px
+}
+#users .uzr.muh1 .My_star1 {
+  width: 64px;
+  margin-top: -58px;
+  margin-left: 9px;
+  height: 60px;
+}
+
+
+`);
+var muhamed=setInterval(function(){
+    if(myid!=null){
+        clearInterval(muhamed);
+        $("#users .uzr:contains('محمد')").addClass('muh1');
+        $("#users .uzr.muh1 .c-flex.fr").append('<img class="My_star1" src="https://f.top4top.io/p_2593paryo0.gif">');
+	
+	$("#users .uzr.muh1 .c-flex.fr").after('<div id="muhameed_style" class="label label-primary Ahmed" style=" margin-bottom: 1px; display: inline-block;width: 100%;margin-top: 10px;height: 22px;cursor: pointer;border-radius: 0px 0px 50px 50px;padding: 0px;background-color: #EDEDED!important;color:#000;" href="https://fhh0.com/">وقتٍ مضى لإيهمك، انتهى مع ناسه</div>');
+        
+    
+        $("#users .uzr.muh1 span.uhash, #users .uzr.muh1 img.co").hide(); 
+
+
+        
+     
+        
+        
+		  
+        
+
+       $("#ifr_mov1").show(function(){
+        $("#upro").modal("hide");
+    } ); 
+});
+    }
+}, 10000);
